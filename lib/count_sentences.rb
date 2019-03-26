0f9d362aafabb1dc10d3ea8c9b5c8e64eec09ada
lieby weintraub <liebahinda@gmail.com>
2019-03-26 22:55:47 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -16,8 +16,15 @@ class String
   end
 
   def count_sentences
+<<<<<<< HEAD
    
   new_array=self.split(/\?|\.|!/).reject{ |c| c == "" }.count
   
+=======
+    binding.pry
+   self.split(/\?|\.|!/).compact
+  
+
+>>>>>>> 53f3c9b5afae0e5135959693f1a10b544f08a483
   end
 end
